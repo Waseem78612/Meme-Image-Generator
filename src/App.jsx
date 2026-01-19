8405c712801ed33239637f97d9ace2a8dedb5d66
Waseem Sajjad <waseem2gk@gmail.com>
2026-01-19 21:32:25 +0500

    Changes Occures to get random meme images

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import { useState, useEffect } from "react"; // ← Import useEffect
 import "./App.css";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log("UseEffect Function is called");
-  }, [count]);
   return (
     <>
-      <button
-        onClick={() => setCount((prevCount) => prevCount + 1)}
-        style={{ height: "120px", width: "120px" }}
-      >
-        Add
-      </button>
-      <h3>This Count is {count}</h3>
       <Header />
       <Main />
     </>
